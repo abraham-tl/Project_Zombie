2d01a1d907b9ecdd0cd88ed9c4b02af9d36b2c43
Abrahan lopera <Abrahan lopera>
2018-04-20 20:58:18 -0500

    Avance de clase
    Avance de clase

--- Project_Zombie/Assets/Scrip/Manager.cs
+++ Project_Zombie/Assets/Scrip/Manager.cs
@@ -3,113 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Manager : MonoBehaviour {
-    //public int enemigos;
     public int numboxes;
     public float spacing;
     public GameObject[] boxes;
     GameObject objecto;
-   
-    // Use this for initialization
+    int edad_;
+    string name_color ="";
     void Start () {
         string[] names = new string[]
         {
             "ABRAHAN","STUBBS","ROB","WHITE","JOSE","WILLIAM","JUANFER","ELKIN","ANDRES","SANTIAGO","FEDRY","ALEJO","VICTOR","SANDRA","LUIS","ISABEL","PEDRO","PABLO ","JASINTO JOSE","ANDREA",
         };
 
-        numboxes = Random.Range(5, 10);
+        numboxes = Random.Range(5, 11);
         boxes = new GameObject[numboxes];
 
         for (int k = 0; k < numboxes; k++)
         {
-
+            edad_ = Random.Range(15, 101);
             int tipo = Random.Range(1, 3);
 
             if (k == 0)
             {
-                //objecto = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-                Zombie z = new Zombie(0, Color.black ,names[0], 30);
-                //objecto.AddComponent(typeof(Zombie));
+                Zombie z = new Zombie(0,Color.black , names[0], edad_);
+                boxes[k] = z.Objeto;
+                objecto = z.Objeto;
+                objecto.AddComponent(typeof(Move_Player));
                 //objecto.AddComponent(typeof(Move_Player));
-
-                //Camera cam = new Camera();
-                ////cam.AddComponent(typeof(Movimiento_Camera));
-
-                ////cam.transform.SetParent(objecto.transform);
-                //objecto.AddComponent<Camera>();
+                objecto.AddComponent<Camera>();
             }
 
             else
             {
-               
+                Color col;               
+                if (tipo == 2)
+                {
+                    col = Color.gray;
+                }
+                else
+                {
+                     col = Colores();
+                }
+                Zombie z = new Zombie(tipo,col,names[k], edad_);
+                boxes[k] = z.Objeto;
                 int otro = Random.Range(1, 2);
-                if (tipo == 1)
-                {
-                    Color coler = Colores();
-                    Zombie z = new Zombie(tipo, coler, names[0], 30);
+                if (tipo == 2)
+                {             
+                    print(z.Name);
                 }
-                else if (tipo ==2)
+                else if (tipo ==1)
                 {
-                    Zombie z = new Zombie(tipo, Color.white , names[0], 30);
+                    print(z.Name + " " + name_color );
                 }
-                
-
-                
                 //objecto = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 //Zombie z = new Zombie(names[1], Random.Range(10, 15), objecto);
             }
-
-            //
-
-            //Debug.Log(z.name);
-        }
-
-
-        for (int k =0;k < numboxes; k++)
-        {
-
-            //GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            //box.AddComponent(typeof(Monster));
-            //Monster m = box.GetComponent("Monster") as Monster;
-            //m.id = k;
-            //m.spacing = spacing;
-            //boxes[k] = box;          
-        }
-        
-        
+        }       
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        //int i = 0;
-        //foreach (GameObject go in boxes)
-        //{
-        //    float wave = Mathf.Sin(Time.fixedTime + i);
-        //    go.transform.position = new Vector3(i * spacing, wave, 0);
-        //    i++;
-        //    print(Time.fixedTime);
-        //}
     }
 
-    void Names()
-    {
-
-        
-        
-    }
     Color Colores()
     {
-        int col = Random.Range(1,4);
+        int col = Random.Range(1, 4);
         if (col == 1)
         {
-            return Color.blue;
+            name_color = "AZUL";
+            return Color.blue;      
         }
         else if (col == 2)
         {
+            name_color = "VERDE";
             return Color.green;
         }
         else if (col == 3)
         {
+            name_color = "ROJO";
             return Color.red;
         }
         else
--- Project_Zombie/Assets/Scrip/Zombie.cs
+++ Project_Zombie/Assets/Scrip/Zombie.cs
@@ -11,32 +11,90 @@ public class Zombie  {
     //public int hitpoints;
    
     
-    public Zombie(int tipo,Color coler,string n,int edad)
+    public Zombie(int tipo,Color col,string n,int edad_)
     {
-        name = n;
-        edad = 0;
-        //hitpoints = hp;
-        objeto = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        edad = edad_; 
+        if (tipo == 0)
+        {
+            objeto = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        }
+        else if(tipo == 1)
+        {
+            objeto = GameObject.CreatePrimitive(PrimitiveType.Cube);    
+            name = "SOY UN ZOMBIE DE COLOR :";
+        }
+        else if (tipo == 2)
+        {
+            objeto = GameObject.CreatePrimitive(PrimitiveType.Cube );
+            name = "HOLA SOY  "+ n + " Y TENGO "+ edad +" AÑOS";
+        }
         Vector3 pos = new Vector3();
         pos.x = Random.Range(-10,10);
         pos.y = 0f;
         pos.z = Random.Range(-10, 10);
         objeto.transform.position = pos;
-        objeto.GetComponent<Renderer>().material.color = coler;
-
-    }
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        objeto.GetComponent<Renderer>().material.color = col;
 
     }
 
-    void colores()
+    public int Tipo
     {
+        set
+        {
+            tipo = value;
+        }
+        get
+        {
+            return tipo;
+        }
+    }
 
+    public Color Color_
+    {
+        set
+        {
+            col = value;
+        }
+        get
+        {
+            return col;
+        }
+    }
+
+    public string Name
+    {
+        set
+        {
+            name = value;
+        }
+        get
+        {
+            return name;
+        }
+    }
+
+    public int Edad
+    {
+        set
+        {
+            edad = value;
+        }
+        get
+        {
+            return edad;
+        }
+      
+    }
+
+    public GameObject Objeto
+    {
+        set
+        {
+            objeto = value;
+        }
+        get
+        {
+            return objeto;
+        }
     }
 }
